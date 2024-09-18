3e46c8ac34efff850c5522dc97404eb052e205ab
Crow_ <pedrojuchimiuk@gmail.com>
2024-09-18 12:48:01 -0300

    Projeto Fiap ODS, NavBar e Search

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Container from './components/container/container'
+import Navbar from './components/navbar/navbar'
+
+
+function App() {
+
+  return (
+    <>
+    <body>
+     <Navbar />
+     <Container />
+     </body>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,43 @@
+import './style.css'
+
+ const Navbar = () => {
+
+    return (
+        
+        <div className="sidebar">
+        <a href='#'>HOME</a> 
+        
+      <div className="side-nav">
+
+        <div className="item active">
+          <i class='bx bx-leaf'></i>
+            <a href='#'>Comunidades</a>
+        </div>
+
+        <div className="item active">
+          <i class='bx bx-objects-vertical-bottom'></i>
+            <a href='#'>Dados de Recurso</a>
+        </div>
+
+        <div className="item">
+          <i class='bx bx-bell'></i>
+            <a href='#'>Notifications</a>
+        </div>
+
+        <div className="item">
+          <i class='bx bx-cog'></i>
+            <a href='#'>Settings</a>
+        </div>
+
+      </div>
+        
+        <div className="side-profile">
+            <i class='bx bx-user' ></i>
+            <a href="#">Usuario</a>
+        </div>
+
+      </div>
+    )
+ }
+
+ export default Navbar

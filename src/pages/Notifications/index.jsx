104640872d71a7449ae8b070381e8ae30327a876
danieltinois <danieltinois@gmail.com>
2024-11-09 09:52:08 -0300

    Atualizações na seção de notificações

--- src/pages/Notifications/index.jsx
+++ src/pages/Notifications/index.jsx
@@ -1,14 +1,39 @@
-import Navbar from '../../components/Navbar'
-import './styles.css'
+import Navbar from '../../components/Navbar';
+import './styles.css';
 
+const notifications = [
+  {
+    id: 1,
+    text: 'Phasellus ultricies ultricies risus id consequat. Proin sit amet scelerisque elit. Vivamus efficitur risus ornare ipsum vehicula luctus.',
+  },
+  {
+    id: 2,
+    text: 'Phasellus ultricies ultricies risus id consequat. Proin sit amet scelerisque elit. Vivamus efficitur risus ornare ipsum vehicula luctus.',
+  },
+  {
+    id: 3,
+    text: 'Phasellus ultricies ultricies risus id consequat. Proin sit amet scelerisque elit. Vivamus efficitur risus ornare ipsum vehicula luctus.',
+  },
+];
 
-function Notifications() {
+const Notifications = () => {
+  return (
+    <>
+    <Navbar />
+    <div className="notifications-container">
+      <h2>Notifications</h2>
+      <div className="notifications-list">
+        {notifications.map((notification) => (
+          <div key={notification.id} className="notification-item">
+            <div className="notification-icon"></div>
+            <div className="notification-text">{notification.text}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+    </>
+    
+  );
+};
 
-    return (
-        <>
-        <Navbar></Navbar>
-        </>
-    )
-}
-
-export default Notifications
+export default Notifications;
